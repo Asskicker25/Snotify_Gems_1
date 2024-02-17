f1351eb68ca839666e148ed925b05ca217a481b0
Asskicker25 <sp68860@gmail.com>
2024-02-17 12:59:09 -0500

    Smart array implemented

---
+++ Snotify_Gems1/Snotify_Gems1/src/Containers/SmartArray.h
@@ -0,0 +1,96 @@
+#pragma once
+
+template <typename T>
+class SmartArray
+{
+
+public:
+	SmartArray();
+	~SmartArray();
+
+	void addAtEnd(T data);
+	T getAt(unsigned int index);
+	unsigned int getSize();
+	void clear();
+	bool isEmpty();
+
+private:
+
+	void Grow();
+
+	const unsigned int mInitialSize = 10;
+
+	unsigned int mCapacity = 0;
+
+	//Also the index to add next
+	unsigned int mSize = 0;
+
+	T* mData = nullptr;
+
+};
+
+template<typename T>
+inline SmartArray<T>::SmartArray()
+{
+	mData = new T[mInitialSize];
+	mCapacity = mInitialSize;
+}
+
+template<typename T>
+inline SmartArray<T>::~SmartArray()
+{
+	delete[] mData;
+}
+
+template<typename T>
+inline void SmartArray<T>::addAtEnd(T data)
+{
+	if ((mSize + 1) > mCapacity) { Grow(); }
+
+	mData[mSize] = data;
+	mSize++;
+}
+
+template<typename T>
+inline T SmartArray<T>::getAt(unsigned int index)
+{
+	return mData[index];
+}
+
+template<typename T>
+inline unsigned int SmartArray<T>::getSize()
+{
+	return mSize;
+}
+
+template<typename T>
+inline void SmartArray<T>::clear()
+{
+	delete[] mData;
+	mSize = 0;
+	mCapacity = mInitialSize;
+}
+
+template<typename T>
+inline bool SmartArray<T>::isEmpty()
+{
+	return mSize == 0;
+}
+
+template<typename T>
+inline void SmartArray<T>::Grow()
+{
+	unsigned int newSize = mCapacity + (mCapacity / 4);
+
+	T* newData = new T[newSize];
+
+	for (int i = 0; i < mCapacity; i++)
+	{
+		newData[i] = mData[i];
+	}
+
+	delete[] mData;
+
+	mData = newData;
+	mCapacity = newSize;
+}
--- Snotify_Gems1/Snotify_Gems1/src/main.cpp
+++ Snotify_Gems1/Snotify_Gems1/src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#include "Containers/SmartArray.h"
+
 int main(int argc, char* argv[])
 {
+
 
 	return -1;
 }
